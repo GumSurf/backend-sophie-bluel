ec7eb887224cad071b9a6b93df3074eb93e0537a
GumSurf <gabriel.christe@gmail.com>
2024-07-10 20:59:08 +0200

    Add: test

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors')
-//require('dotenv').config();
+require('dotenv').config();
 const helmet = require('helmet');
-const swaggerUi = require('swagger-ui-express')
-const yaml = require('yamljs')
-const swaggerDocs = yaml.load('swagger.yaml')
+//const swaggerUi = require('swagger-ui-express')
+//const yaml = require('yamljs')
+//const swaggerDocs = yaml.load('swagger.yaml')
 const app = express()
 
 app.use((req, res, next) => {
