c5dd8209edb80c24c40f360c8dc87c07c1217e9d
GumSurf <gabriel.christe@gmail.com>
2024-07-10 20:23:30 +0200

    Add: test

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const path = require('path');
 const cors = require('cors')
 require('dotenv').config();
@@ -27,4 +27,26 @@ app.get('/', (req, res) => {
 app.use('/api/users', userRoutes);
 app.use('/api/categories', categoriesRoutes);
 app.use('/api/works', worksRoutes);
+module.exports = app;*/
+
+const express = require('express');
+
+const app = express();
+
+//////////////////////////////////
+// permettre l'accès à l'API (CORS)
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token,Origin, X-Requested-With, Content, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  
+  app.use((req, res) => {
+    res.json({ message: "UPDATE !" }); 
+ });
+
 module.exports = app;
