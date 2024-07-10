807dbe3851ec12d46feb5d10c62e21b6a77a8ca4
GumSurf <gabriel.christe@gmail.com>
2024-07-10 21:00:03 +0200

    Add: test

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
     next();
   });
 
-/*app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(helmet({
@@ -36,10 +36,6 @@ app.get('/', (req, res) => {
   });
 app.use('/api/users', userRoutes);
 app.use('/api/categories', categoriesRoutes);
-app.use('/api/works', worksRoutes);*/
-
-app.use((req, res) => {
-    res.json({ message: "UPDATE !" }); 
- });
+app.use('/api/works', worksRoutes);
 
 module.exports = app;
