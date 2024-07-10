c155a09c1a7b60f101e60fbf403667a53201fb32
GumSurf <gabriel.christe@gmail.com>
2024-07-10 21:05:38 +0200

    Add: test

--- app.js
+++ app.js
@@ -25,13 +25,13 @@ app.use(helmet({
     }));
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
-/*const db = require("./models");
+const db = require("./models");
 const userRoutes = require('./routes/user.routes');
 const categoriesRoutes = require('./routes/categories.routes');
 const worksRoutes = require('./routes/works.routes');
 db.sequelize.sync().then(()=> console.log('db is ready'));
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     res.send('Bienvenue sur le serveur de Sophie Bluel !');
   });
 app.use('/api/users', userRoutes);
